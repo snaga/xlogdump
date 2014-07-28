d95a2d7149d030c3232c896159a8afdf6f230815
Billy Hand <bhand@mediamath.com>
2014-07-28 01:02:46 -0500

    path fix

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	entries := walparse.ParseWalFile("000000010000000000000001", 17812976);
+	entries := walparse.ParseWalFile("xlogtranslate/000000010000000000000001", 17812976);
 	for i := range(entries) {
 		fmt.Println(entries[i])
 	}
