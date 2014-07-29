6bff132562c9a98fa7b1112e392ce5234d01b069
Billy Hand <bhand@mediamath.com>
2014-07-29 03:00:23 -0500

    proper transaction publishing

--- main.go
+++ main.go
@@ -10,52 +10,70 @@ import (
 )
 
 /*
-read files sequentially
-retain backlog
-on commit clear backlog of all committed updates
-periodically drop old records based on some limit
+ fix malloc usage
+ publish updates over sockets (maybe amqp)
+ test using postgres user
  */
 
 func watchWalDirectory(path string, quitChan chan bool) (<-chan string) {
-	var highestLogId, highestRecOff uint32 = 0, 0
-
 	// this channel is returned by this method to send updates to clients
 	commitsChan := make(chan string)
 
 	// this channel passes entries to be processed by the first go routine
 	entryChan := make(chan walparse.WalEntry)
 
-	// process entries as they come in and generate messages on the commitsChan
-	go func() {
-		for entry := range entryChan {
-			fmt.Sprintf("%v", entry)
-			commitsChan <- fmt.Sprintf("%v %v", entry.XLogId, entry.XRecOff)
-		}
-	}()
-
-	// read current files
-	files, err := filepath.Glob(path+"*")
-	if err != nil {
-	    log.Fatal(err)
-	}
-
-	// ... in order
-	sort.Sort(sort.StringSlice(files))
-
-	// ... and publish to entryChan
 	var publishEntries = func(filename string) {
 		entries := walparse.ParseWalFile(filename, 0)
 		for _, e := range entries {
-			if (e.XLogId > highestLogId || (e.XLogId == highestLogId && e.XRecOff > highestRecOff)) {
-				entryChan <- e
-				highestLogId, highestRecOff = e.XLogId, e.XRecOff
-			}
+			entryChan <- e
 		}
 	}
 
-	for _, f := range files {
-		publishEntries(f)
-	}
+	// process entries as they come in and generate messages on the commitsChan
+	go func() {
+		var highestLogId, highestRecOff uint32 = 0, 0
+		var entries = make([]walparse.WalEntry, 0)
+
+		for entry := range entryChan {
+			if (entry.XLogId > highestLogId || (entry.XLogId == highestLogId && entry.XRecOff > highestRecOff)) {
+				highestLogId, highestRecOff = entry.XLogId, entry.XRecOff
+
+				switch {
+				case entry.RmId == walparse.RM_XACT_ID && entry.Info == walparse.XLOG_XACT_COMMIT:
+					temp := entries
+					entries = make([]walparse.WalEntry, 0)
+					for _, old := range temp {
+						if old.XId == entry.XId {
+							commitsChan <- fmt.Sprintf("committed:%v-%v as part of XId:%v", old.XLogId, old.XRecOff, old.XId)
+						} else {
+							entries = append(entries, old)
+						}
+					}
+				case entry.RmId == walparse.RM_HEAP_ID:
+					var heapOp = entry.Info & 0x70
+					if heapOp == 0x00 || heapOp == 0x10 || heapOp == 0x20 || heapOp == 0x40 {
+						entries = append(entries, entry)
+					}
+				}
+			}
+		}
+	}()
+
+	go func() {
+		// read current files
+		files, err := filepath.Glob(path+"*")
+		if err != nil {
+		    log.Fatal(err)
+		}
+
+		// ... in order
+		sort.Sort(sort.StringSlice(files))
+
+		// ... and publish to entryChan
+		for _, f := range files {
+			publishEntries(f)
+		}
+	}()
 
 	// now begin watching for changes to files and publish to entryChan
 	watcher, err := fsnotify.NewWatcher()
@@ -69,7 +87,9 @@ func watchWalDirectory(path string, quitChan chan bool) (<-chan string) {
 	    for {
 	        select {
 	        case event := <-watcher.Events:
-	        	publishEntries(event.Name)
+	        	if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
+		        	publishEntries(event.Name)
+		        }
 	        case err := <-watcher.Errors:
 	            log.Println("error:", err)
 	        case <-quitChan:
@@ -92,7 +112,7 @@ func main() {
 	commitsChan := watchWalDirectory("./xlog/", quitChan)
 
 	for commit := range commitsChan {
-		log.Println(commit)
+		fmt.Printf("%v\n", commit)
 	}
 
 	<-quitChan
